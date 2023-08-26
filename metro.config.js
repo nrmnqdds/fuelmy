43ed40b2cd07a270ef9fbb733939101e94f09cd0
qryskalyst20 <quddusnuriman@gmail.com>
2023-08-27 03:45:53 +0800

    :ambulance: Configure metro to accept ts file

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,9 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname, {
   isCSSEnabled: true,
+  resolver: {
+    sourceExts: ["js", "ts", "tsx", "svg"],
+  },
 });
 
 module.exports = config;
